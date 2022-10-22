703cf57b12bce06b327327e98e79649beb49944d
Luba <bart6891@ya.ru>
2022-10-22 13:23:03 +0300

    the sum of the elements standing in odd positions1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 0; j < numbers.Length; j+=2)
+    sum = sum + numbers[j];
+    Console.WriteLine($"сумма элементов cтоящих на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(-100,100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
+
